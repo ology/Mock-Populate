f1ec1da04f76e045d3eaf3161cf16b73e7d42a82
Gene Boggs <gene.boggs@gmail.com>
2013-12-12 13:58:07 -0800

    Stubbed equivalent script functions. Improved docs

--- lib/Mock/Populate.pm
+++ lib/Mock/Populate.pm
@@ -19,11 +19,11 @@ Mock::Populate - Handy mock data creation
 
 =head1 SYNOPSIS
 
-> perl Personify b 2 1000 > people.txt
-> perl Date-Ranger 1900-01-01 2020-12-31 1000 > dates.txt
-> perl Number-Ranger 1000 5000 2 1000 > nums.txt
-> perl Stats-Distrib n 3 1000 > stats.txt
-> perl Columns people.txt dates.txt nums.txt stats.txt > mock-data.txt
+  > perl Date-Ranger 1900-01-01 2020-12-31 1000 > dates.txt
+  > perl Number-Ranger 1000 5000 2 1000 > nums.txt
+  > perl Personify b 2 1000 > people.txt
+  > perl Stats-Distrib n 3 1000 > stats.txt
+  > perl Collate people.txt dates.txt nums.txt stats.txt > mock-data.txt
 
 =head1 DESCRIPTION
 
@@ -31,8 +31,56 @@ This is a set of modules and scripts handy for mock data creation.
 
 See the documentation (and source) of each script for arguments and usage.
 
+=head1 FUNCTIONS
+
+=head2 date_ranger()
+
 =cut
+
+sub date_ranger {
+}
+
+=head2 number_ranger()
+
+=cut
+
+sub number_ranger {
+}
+
+=head2 personify()
+
+=cut
+
+sub personify {
+}
+
+=head2 stats_distrib()
+
+=cut
+
+sub stats_distrib {
+}
+
+=head2 collate()
+
+=cut
+
+sub collate { # 
+}
 
 1;
 __END__
 
+=head1 SEE ALSO
+
+L<Data::SimplePassword>
+
+L<Date::Range>
+
+L<Mock::Person>
+
+L<Statistics::Distribution>
+
+L<List::Util>
+
+=cut
